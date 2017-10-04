9e093e5680b08c32cc749c886bbccd2dca708cb6
ryanlapeyre <ryanlapeyre@gmail.com>
2017-10-03 19:26:52 -0500

    another react test

--- app/javascript/packs/file.jsx
+++ app/javascript/packs/file.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import {Page, Card, Button, Thumbnail} from '@shopify/polaris';
-const Hello = props => (
-<DisplayText size="large">Good evening, Dominic.</DisplayText>
-)
+import '@shopify/polaris/styles.css';
 
+const Hello2 = props => (
+  <DisplayText size="large">Good evening, Dominic.</DisplayText>
+)
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello/>,
-    document.body.appendChild(document.createElement('DisplayText')),
+    <Hello name="React" />,
+    document.body.appendChild(document.createElement('div')),
   )
 })
