c40ff985eff701b7fba5a588d4e44e526e65c7b2
ryanlapeyre <ryanlapeyre@gmail.com>
2017-10-04 16:21:21 -0500

    buttonWorked reset

---
+++ app/javascript/packs/test.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import {DisplayText} from '@shopify/polaris';
+import '@shopify/polaris/styles.css';
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const domContainerNode = document.getElementById('test')
+    ReactDOM.render(
+      <DisplayText size="extraLarge">Good evening, Dominic.</DisplayText>
+      , domContainerNode);
+  })
