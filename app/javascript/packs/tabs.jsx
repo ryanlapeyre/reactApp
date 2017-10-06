d69e01174d5f21213e04abf3230806c58f24712b
ryanlapeyre <ryanlapeyre@gmail.com>
2017-10-05 23:20:03 -0500

    added tabS fix

--- app/javascript/packs/tabs.jsx
+++ app/javascript/packs/tabs.jsx
@@ -1,44 +1,66 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
 import {Tabs} from '@shopify/polaris';
 import '@shopify/polaris/styles.css';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.handleTabChange = this.handleTabChange.bind(this);
 
+    this.state = {
+      selectedTab: 0,
+    };
+  }
+
+  handleTabChange(selectedTab) {
+    this.setState({selectedTab});
+  }
+
+  render() {
+    const {selectedTab} = this.state;
+
+    const tabs = [
+      {
+        id: 'tab1',
+        title: 'something1',
+        panelID: 'panel2',
+      },
+      {
+        id: 'tab2',
+        title: 'something2',
+        panelID: 'panel2',
+      },
+    ];
+
+    const tabPanels = [
+      (
+        <Tabs.Panel id="panel1">
+          something
+        </Tabs.Panel>
+      ),
+      (
+        <Tabs.Panel id="panel2">
+          something else
+        </Tabs.Panel>
+      ),
+    ];
+
+    return (
+      <div>
+        <Tabs
+          selected={selectedTab}
+          tabs={tabs}
+          onSelect={this.handleTabChange}
+        />
+        {tabPanels[selectedTab]}
+      </div>
+    );
+  }
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const domContainerNode = document.getElementById('tabs')
-    ReactDOM.render(
-
-  <Tabs
-    selected={0}
-    tabs={[
-      {
-        id: 'all-customers',
-        title: 'All',
-        accessibilityLabel: 'All customers',
-        panelID: 'all-customers-content',
-      },
-      {
-        id: 'accepts-marketing',
-        title: 'Accepts marketing',
-        panelID: 'accepts-marketing-content',
-      },
-      {
-        id: 'repeat-customers',
-        title: 'Repeat customers',
-        panelID: 'repeat-customers-content',
-      },
-      {
-        id: 'prospects',
-        title: 'Prospects',
-        panelID: 'prospects-content',
-      },
-    ]}
-  />
-
-      , domContainerNode);
+    ReactDOM.render(App, domContainerNode);
   })
