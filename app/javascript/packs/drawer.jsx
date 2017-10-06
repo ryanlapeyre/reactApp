11ba98520f2cb2c5aeb112466a756557c9c1acb9
ryanlapeyre <ryanlapeyre@gmail.com>
2017-10-06 13:32:01 -0500

    added drawer

---
+++ app/javascript/packs/drawer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+
+class DrawerSimpleExample extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  render() {
+    return (
+      <div>
+        <RaisedButton
+          label="Toggle Drawer"
+          onClick={this.handleToggle}
+        />
+        <Drawer open={this.state.open}>
+          <MenuItem>Menu Item</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+        </Drawer>
+      </div>
+    );
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const domContainerNode = document.getElementById('drawer')
+    ReactDOM.render( DrawerSimpleExample, domContainerNode);
+  })
