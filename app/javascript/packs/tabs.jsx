471ca760557ea1dce27c656fefd8582aeb2873df
ryanlapeyre <ryanlapeyre@gmail.com>
2017-10-06 00:17:58 -0500

    tabs fix hopefully

--- app/javascript/packs/tabs.jsx
+++ app/javascript/packs/tabs.jsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 import React, { Component } from 'react';
 import {Tabs, Page, Card, Button, Thumbnail} from '@shopify/polaris';
 import '@shopify/polaris/styles.css';
@@ -37,27 +38,25 @@ class App extends Component {
     const tabPanels = [
       (
         <Tabs.Panel id="panel1">
-        const Hello = props => (
-           <Page title="Products">
-            {props.products.map((product, index) => (
-            <Card key={index}
-              title={product.title}
-              primaryFooterAction={{
-                content: 'View',
-                url: 'https://${shop_session.url}/admin/products/${product.id}',
-              }}
-              sectioned
-            >
-                <Thumbnail
-                  source={product.images[0].src}
-                  alt={product.title}
-                  size="large"
-                />
+        <Page title="Products">
+         {props.products.map((product, index) => (
+         <Card key={index}
+           title={product.title}
+           primaryFooterAction={{
+             content: 'View',
+             url: 'https://${shop_session.url}/admin/products/${product.id}',
+           }}
+           sectioned
+         >
+             <Thumbnail
+               source={product.images[0].src}
+               alt={product.title}
+               size="large"
+             />
 
-            </Card>
-            ))}
-          </Page>
-        )
+         </Card>
+         ))}
+       </Page>
         </Tabs.Panel>
       ),
       (
@@ -79,7 +78,6 @@ class App extends Component {
     );
   }
 }
-
 
 
 document.addEventListener('DOMContentLoaded', () => {
