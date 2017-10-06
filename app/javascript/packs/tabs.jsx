35c81993615d41486a60f3c1dd05969885a82091
ryanlapeyre <ryanlapeyre@gmail.com>
2017-10-05 22:33:28 -0500

    added tabs fix

---
+++ app/javascript/packs/tabs.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import {Tabs} from '@shopify/polaris';
+import '@shopify/polaris/styles.css';
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const domContainerNode = document.getElementById('test')
+    ReactDOM.render(
+
+  <Tabs
+    selected={0}
+    tabs={[
+      {
+        id: 'all-customers',
+        title: 'All',
+        accessibilityLabel: 'All customers',
+        panelID: 'all-customers-content',
+      },
+      {
+        id: 'accepts-marketing',
+        title: 'Accepts marketing',
+        panelID: 'accepts-marketing-content',
+      },
+      {
+        id: 'repeat-customers',
+        title: 'Repeat customers',
+        panelID: 'repeat-customers-content',
+      },
+      {
+        id: 'prospects',
+        title: 'Prospects',
+        panelID: 'prospects-content',
+      },
+    ]}
+  />
+
+      , domContainerNode);
+  })
