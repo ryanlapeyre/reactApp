99c0299902a97f32f92e4f668ee0f90d325fe849
ryanlapeyre <ryanlapeyre@gmail.com>
2017-10-06 01:10:03 -0500

    tab reset

--- app/javascript/packs/tabs.jsx
+++ app/javascript/packs/tabs.jsx
@@ -38,25 +38,7 @@ class App extends Component {
     const tabPanels = [
       (
         <Tabs.Panel id="panel1">
-        <Page title="Products">
-         {props.products.map((product, index) => (
-         <Card key={index}
-           title={product.title}
-           primaryFooterAction={{
-             content: 'View',
-             url: 'https://${shop_session.url}/admin/products/${product.id}',
-           }}
-           sectioned
-         >
-             <Thumbnail
-               source={product.images[0].src}
-               alt={product.title}
-               size="large"
-             />
-
-         </Card>
-         ))}
-       </Page>
+          something
         </Tabs.Panel>
       ),
       (
